4ebc2b5d1dc01272991da1fa79f6584ea447087a
Arturo Mendoza <arturocmi91@gmail.com>
2023-05-14 20:53:38 +0200

    updating tables

--- Medical-Equiment-Web/src/main/java/ironhack/com/MedicalEquiment/Web/models/Employee.java
+++ Medical-Equiment-Web/src/main/java/ironhack/com/MedicalEquiment/Web/models/Employee.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED )
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS )
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -18,14 +18,19 @@ public class Employee {
     @OneToMany(mappedBy = "employee")
     private List<CustomerOrder> orders;
 
+    @ManyToOne
+    @JoinColumn(name="manager_id")
+    private Manager manager;
+
 
     public Employee() {
     }
 
-    public Employee(String employeeName, String employeeEmail, List<CustomerOrder> orders) {
+    public Employee(String employeeName, String employeeEmail, List<CustomerOrder> orders, Manager manager) {
         this.employeeName = employeeName;
         this.employeeEmail = employeeEmail;
         this.orders = orders;
+        this.manager = manager;
     }
 
     public Long getId() {
@@ -60,16 +65,24 @@ public class Employee {
         this.orders = orders;
     }
 
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Employee employee = (Employee) o;
-        return Objects.equals(id, employee.id) && Objects.equals(employeeName, employee.employeeName) && Objects.equals(employeeEmail, employee.employeeEmail) && Objects.equals(orders, employee.orders);
+        return Objects.equals(id, employee.id) && Objects.equals(employeeName, employee.employeeName) && Objects.equals(employeeEmail, employee.employeeEmail) && Objects.equals(orders, employee.orders) && Objects.equals(manager, employee.manager);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, employeeName, employeeEmail, orders);
+        return Objects.hash(id, employeeName, employeeEmail, orders, manager);
     }
 }
--- Medical-Equiment-Web/src/main/java/ironhack/com/MedicalEquiment/Web/models/Manager.java
+++ Medical-Equiment-Web/src/main/java/ironhack/com/MedicalEquiment/Web/models/Manager.java
@@ -3,6 +3,7 @@ package ironhack.com.MedicalEquiment.Web.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Objects;
 
@@ -12,14 +13,17 @@ public class Manager extends Employee {
     @OneToMany(mappedBy = "manager")
     private List<Customer> customers;
 
+    @OneToMany(mappedBy = "manager")
+    private List<Employee> employees;
+
 
     public Manager() {
     }
 
-    public Manager(String employeeName, String employeeEmail, List<CustomerOrder> orders, List<Customer> customers) {
-        super(employeeName, employeeEmail, orders);
+    public Manager(String employeeName, String employeeEmail, List<CustomerOrder> orders, Manager manager, List<Customer> customers, List<Employee> employees) {
+        super(employeeName, employeeEmail, orders, manager);
         this.customers = customers;
-
+        this.employees = employees;
     }
 
     public List<Customer> getCustomers() {
@@ -30,17 +34,25 @@ public class Manager extends Employee {
         this.customers = customers;
     }
 
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         Manager manager = (Manager) o;
-        return Objects.equals(customers, manager.customers);
+        return Objects.equals(customers, manager.customers) && Objects.equals(employees, manager.employees);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), customers);
+        return Objects.hash(super.hashCode(), customers, employees);
     }
 }
